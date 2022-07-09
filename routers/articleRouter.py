7f9546ed031e71a2e2522903f1e7539075a06c3a
nilldiggonot <nilldiggonto@gmail.com>
2022-07-09 18:18:07 +0000

    async crud done

--- routers/articleRouter.py
+++ routers/articleRouter.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,Response,Body
 from database import db
 from models import Article
 from sqlalchemy.future import select
+from sqlalchemy import update,delete
 
 router = APIRouter(prefix="/article")
 
@@ -16,10 +17,26 @@ async def createArticle(request:dict=Body(...)):
     # await db.execute(query)
     return {'status':'created'}
 
-@router.get('/articles')
+@router.get('/all')
 async def fetchArticle():
     query = select(Article)
     query = await db.execute(query)
     query = query.scalars().all()
-    data = [{'id':q.id,'title':q.title,'description':q.description} for q in query]
+    data = [{'id':q.id,'title':q.title,'description':q.description,'updated':q.updated} for q in query]
     return data
+
+@router.put('/update/{id}')
+async def updateArticle(id:int,request:dict=Body(...)):
+
+    query = update(Article).where(Article.id==id)
+    query = query.values(title=request['title'],description=request['description'])  
+    query.execution_options(synchronize_session="fetch")
+    await db.execute(query)
+    # await db.commit()    
+    return {'status':'updated'}
+
+@router.delete('/delete/{id}')
+async def deleteArticle(id:int):
+    query = delete(Article).where(Article.id == id)
+    await db.execute(query)
+    return {'status':'deleted'}
